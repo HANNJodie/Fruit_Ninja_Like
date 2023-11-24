e8e015c92458b028e1525c6d932e92625ebd6e26
Jodie HANN <117754974+HANNJodie@users.noreply.github.com>
2023-11-24 12:51:19 +0100

    Debut du start screen

---
+++ Assets/Script/Difficulty_controller.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Difficulty_controller : MonoBehaviour
+
+{
+
+    private Button button;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(SetDifficulty);
+    }
+
+    void SetDifficulty()
+    {
+        Debug.Log(gameObject.name + "was clicked");
+    }
+}
